ce15aa01d602806b742b0eb073d3973ee45fae41
RadostW <radost.waszkiewicz@gmail.com>
2025-05-21 11:58:45 +0200

    WIP sample fit

--- fit_simple.py
+++ fit_simple.py
@@ -1,4 +1,16 @@
 import pycorrfit
+import matplotlib.pyplot as plt
 
 data_path = "sample_data/sample.fcs"
 data = pycorrfit.readfiles.open_any(data_path)
+
+for trace in data['Trace']:
+    plt.plot(trace[:,0],trace[:,1])
+
+plt.show()
+
+for trace in data['Correlation']:
+    plt.plot(trace[:,0],trace[:,1])
+plt.xscale('log')  # Set x-axis to logarithmic scale
+
+plt.show()
